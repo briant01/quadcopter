ca5ecb99305da3acd16978b5ef18df6b8b4f8bfb
Brian <brian.q.tram@gmail.com>
2024-06-07 13:45:00 -0400

    gain mac address from esps

--- quadcopter/quadcopter.ino
+++ quadcopter/quadcopter.ino
@@ -1,9 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "WiFi.h"
 
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  Serial.println(WiFi.macAddress());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
