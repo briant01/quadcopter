8c529e18d55c1e38e17f557a75a00f4bcacd48df
Brian <brian.q.tram@gmail.com>
2024-06-20 14:22:52 -0400

    accelerometer and gyroscope

--- quadcopter/quadcopter.ino
+++ quadcopter/quadcopter.ino
@@ -2,13 +2,22 @@
   Rui Santos & Sara Santos - Random Nerd Tutorials
   Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/  
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
-//21/SDA = 
+Adafruit_MPU6050 mpu;
+
+// Define offsets for accelerometer and gyroscope
+float accelOffsetX = 1.43;
+float accelOffsetY = -0.2;
+float accelOffsetZ = 10;
+float gyroOffsetX = -0.06;
+float gyroOffsetY = 0.02;
+float gyroOffsetZ = -0.01;
 
 // Structure example to receive data
 // Must match the sender structure
@@ -37,11 +46,83 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(myData.valueRightY);
   Serial.println();
 }
- 
+
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -54,8 +135,45 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
-}
- 
-void loop() {
 
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Apply offsets to the raw sensor data */
+  float accelX = a.acceleration.x - accelOffsetX;
+  float accelY = a.acceleration.y - accelOffsetY;
+  float accelZ = a.acceleration.z - accelOffsetZ;
+  float gyroX = g.gyro.x - gyroOffsetX;
+  float gyroY = g.gyro.y - gyroOffsetY;
+  float gyroZ = g.gyro.z - gyroOffsetZ;
+
+  /* Print out the values with offsets applied */
+  Serial.print("Acceleration X: ");
+  Serial.print(accelX);
+  Serial.print(", Y: ");
+  Serial.print(accelY);
+  Serial.print(", Z: ");
+  Serial.print(accelZ);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(gyroX);
+  Serial.print(", Y: ");
+  Serial.print(gyroY);
+  Serial.print(", Z: ");
+  Serial.print(gyroZ);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
 }
